b1cad992ac053945774bec345b2777d5c522574c
Amine-92-03 <mefti.amine92@gmail.com>
2022-08-24 03:14:15 +0200

    ...

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 require('dotenv').config()
 
-app.get('/api',(req, res) =>{
-    console.log('in app.get');
-    return res.status(200).json({data : 1})
-})
+// app.get('/api',(req, res) =>{
+//     console.log('in app.get');
+//     return res.status(200).json({data : 1})
+// })
 
 app.use(express.static('./frontend'));
 module.exports = app

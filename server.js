d4c8185c9e2739ee687ae3cabf2ff75336e505f7
Amine-92-03 <mefti.amine92@gmail.com>
2022-08-24 02:05:33 +0200

    ...

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
 
-app.set('port', process.env.PORT || process.env.PORT)
-server.listen(process.env.PORT || process.env.PORT, (err)=>{
+app.set('port', process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000, (err)=>{
     if(!err){
         console.log('Listen to port:', process.env.PORT);
     }

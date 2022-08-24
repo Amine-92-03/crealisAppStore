2de2f1f8bd1208e83fda166a7c8029c02ada9fd0
Amine-92-03 <mefti.amine92@gmail.com>
2022-08-24 02:12:21 +0200

    ...

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -8,7 +8,7 @@ submiBtn.onclick = async ()=>{
     const form = document.getElementById('formulaire')
     const val = document.querySelectorAll('input')
     op
-    const data = await fetch('http://localhost:3200/api')
+    const data = await fetch('/api')
     const json = await data.json()
     console.log(json);
 }
